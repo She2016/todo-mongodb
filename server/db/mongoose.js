32f1affd496e97109248e1f0bdb522c5e1c82d8b
Sheyar <ali.sheyar@gmail.com>
2018-10-21 12:13:15 +0200

    Setupdbp for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://todo-app:todo-app123@ds137643.mlab.com:37643/todo-app' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.Mongo_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {mongoose}
