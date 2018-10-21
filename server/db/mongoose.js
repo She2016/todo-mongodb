544810b8d3fd9afb210d67fafd93f7d5e8cdf696
Sheyar <ali.sheyar@gmail.com>
2018-10-21 17:03:38 +0200

    Setting Patch router

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://todo-app:todo-app123@ds137643.mlab.com:37643/todo-app' || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {mongoose}
