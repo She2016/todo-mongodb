e2f5b06a8e1269a4ea51060fb4fc1ec8958f50d9
Sheyar <ali.sheyar@gmail.com>
2018-10-21 23:57:19 +0200

    Setting evn variables

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
+//require('dotenv').config()
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://todo-app:todo-app123@ds137643.mlab.com:37643/todo-app' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports = {mongoose}
